eab4111e7881240b34a5158122b86470a1d68c3c
Max <stigerok555@gmail.com>
2025-09-09 09:31:57 +0200

    changed some styles in Header and added burger menu on 600 px

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,24 +1,32 @@
+import { useState, useRef, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import styles from "../header/styles.module.css";
 import mainLogo from "../../assets/icons/mainLogo.svg";
 import basketLogo from "../../assets/icons/basket.svg";
-import { NavLink, useLocation } from "react-router-dom";
 
 function Header() {
-  const location = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const getNavLinkClass = ({ isActive }) =>
     isActive ? `${styles.navLink} ${styles.activeLink}` : styles.navLink;
 
-  const handleLogoClick = () => {
-    if (location.pathname === "/") {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    }
-  };
-
   return (
     <header className={styles.header}>
       <div className={styles.mainLogoContainer}>
-        <NavLink to="/" onClick={handleLogoClick}>
+        <NavLink to="/">
           <img src={mainLogo} alt="mainLogo" />
         </NavLink>
       </div>
@@ -34,6 +42,46 @@ function Header() {
           All products
         </NavLink>
         <NavLink to="/sales" className={getNavLinkClass}>
+          All sales
+        </NavLink>
+      </div>
+
+      <div className={styles.burgerMenu} onClick={() => setMenuOpen(!menuOpen)}>
+        <div className={`${styles.bar} ${menuOpen ? styles.bar1 : ""}`}></div>
+        <div className={`${styles.bar} ${menuOpen ? styles.bar2 : ""}`}></div>
+        <div className={`${styles.bar} ${menuOpen ? styles.bar3 : ""}`}></div>
+      </div>
+
+      <div
+        ref={menuRef}
+        className={`${styles.mobileMenu} ${menuOpen ? styles.showMenu : ""}`}
+      >
+        <NavLink
+          to="/"
+          className={getNavLinkClass}
+          onClick={() => setMenuOpen(false)}
+        >
+          Main Page
+        </NavLink>
+        <NavLink
+          to="/categories"
+          className={getNavLinkClass}
+          onClick={() => setMenuOpen(false)}
+        >
+          Categories
+        </NavLink>
+        <NavLink
+          to="/products"
+          className={getNavLinkClass}
+          onClick={() => setMenuOpen(false)}
+        >
+          All products
+        </NavLink>
+        <NavLink
+          to="/sales"
+          className={getNavLinkClass}
+          onClick={() => setMenuOpen(false)}
+        >
           All sales
         </NavLink>
       </div>
